253b8e359ced87641594083640a75fa11b5c1117
Val Okafor <okaatech@yahoo.com>
2017-02-16 08:57:25 -0800

    Add realm and model classes

--- app/src/main/java/com/okason/prontoquotes/models/Author.java
+++ app/src/main/java/com/okason/prontoquotes/models/Author.java
@@ -1,8 +1,68 @@
 package com.okason.prontoquotes.models;
 
+import io.realm.RealmList;
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+
 /**
  * Created by Valentine on 2/16/2017.
  */
 
-public class Author {
+public class Author extends RealmObject{
+    @PrimaryKey
+    private long id;
+    private String guid;
+    private String authorName;
+    private String authorLocalImageUrl;
+    private String authorCloudImageUrl;
+    private RealmList<Quote> quotes;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getGuid() {
+        return guid;
+    }
+
+    public void setGuid(String guid) {
+        this.guid = guid;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+
+    public String getAuthorLocalImageUrl() {
+        return authorLocalImageUrl;
+    }
+
+    public void setAuthorLocalImageUrl(String authorLocalImageUrl) {
+        this.authorLocalImageUrl = authorLocalImageUrl;
+    }
+
+    public String getAuthorCloudImageUrl() {
+        return authorCloudImageUrl;
+    }
+
+    public void setAuthorCloudImageUrl(String authorCloudImageUrl) {
+        this.authorCloudImageUrl = authorCloudImageUrl;
+    }
+
+    public RealmList<Quote> getQuotes() {
+        return quotes;
+    }
+
+    public void setQuotes(RealmList<Quote> quotes) {
+        this.quotes = quotes;
+    }
 }
--- app/src/main/java/com/okason/prontoquotes/models/Quote.java
+++ app/src/main/java/com/okason/prontoquotes/models/Quote.java
@@ -1,8 +1,84 @@
 package com.okason.prontoquotes.models;
 
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+
 /**
  * Created by Valentine on 2/16/2017.
  */
 
-public class Quote {
+public class Quote extends RealmObject{
+    @PrimaryKey
+    private long id;
+    private String guid;
+    private String quote;
+    private Author author;
+    private Category category;
+    private String quoteLocalImageUrl;
+    private String quoteCloudImageUrl;
+    private boolean isFavourite;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getGuid() {
+        return guid;
+    }
+
+    public void setGuid(String guid) {
+        this.guid = guid;
+    }
+
+    public String getQuote() {
+        return quote;
+    }
+
+    public void setQuote(String quote) {
+        this.quote = quote;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public String getQuoteLocalImageUrl() {
+        return quoteLocalImageUrl;
+    }
+
+    public void setQuoteLocalImageUrl(String quoteLocalImageUrl) {
+        this.quoteLocalImageUrl = quoteLocalImageUrl;
+    }
+
+    public String getQuoteCloudImageUrl() {
+        return quoteCloudImageUrl;
+    }
+
+    public void setQuoteCloudImageUrl(String quoteCloudImageUrl) {
+        this.quoteCloudImageUrl = quoteCloudImageUrl;
+    }
+
+    public boolean isFavourite() {
+        return isFavourite;
+    }
+
+    public void setFavourite(boolean favourite) {
+        isFavourite = favourite;
+    }
 }
