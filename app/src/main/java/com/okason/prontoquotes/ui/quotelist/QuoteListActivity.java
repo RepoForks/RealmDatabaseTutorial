52402fc3257ff4ca4855ec99d9d9697e69b4ff21
Val Okafor <okaatech@yahoo.com>
2017-02-17 16:01:37 -0800

    Add navigation drawer

--- app/src/main/java/com/okason/prontoquotes/ui/quotelist/QuoteListActivity.java
+++ app/src/main/java/com/okason/prontoquotes/ui/quotelist/QuoteListActivity.java
@@ -1,5 +1,6 @@
 package com.okason.prontoquotes.ui.quotelist;
 
+import android.app.Activity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
@@ -11,6 +12,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import com.mikepenz.fontawesome_typeface_library.FontAwesome;
+import com.mikepenz.google_material_typeface_library.GoogleMaterial;
+import com.mikepenz.materialdrawer.AccountHeader;
+import com.mikepenz.materialdrawer.AccountHeaderBuilder;
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.Nameable;
+import com.mikepenz.materialdrawer.util.KeyboardUtil;
 import com.okason.prontoquotes.R;
 import com.okason.prontoquotes.data.SampleData;
 import com.okason.prontoquotes.utils.Constants;
@@ -19,6 +30,11 @@ public class QuoteListActivity extends AppCompatActivity {
 
     private SharedPreferences sharedPreferences;
     private SharedPreferences.Editor editor;
+    private Toolbar toolbar;
+    private Activity mActivity;
+
+    private AccountHeader mHeader = null;
+    private Drawer mDrawer = null;
 
 
 
@@ -28,11 +44,13 @@ public class QuoteListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quote_list);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
         editor = sharedPreferences.edit();
+        mActivity = this;
         addDefaultData();
+        setupNavigationDrawer(savedInstanceState);
 
 
 
@@ -46,6 +64,59 @@ public class QuoteListActivity extends AppCompatActivity {
             }
         });
         openFragment(new QuoteListFragment(), "Motivational Quotes");
+    }
+
+    private void setupNavigationDrawer(Bundle savedInstanceState) {
+        mHeader = new AccountHeaderBuilder()
+                .withActivity(this)
+                .withHeaderBackground(R.drawable.header)
+                .build();
+
+        mDrawer = new DrawerBuilder()
+                .withAccountHeader(mHeader)
+                .withActivity(this)
+                .withToolbar(toolbar)
+                .withActionBarDrawerToggle(true)
+                .addDrawerItems(
+                        new PrimaryDrawerItem().withName("Quotes").withIcon(GoogleMaterial.Icon.gmd_quote).withIdentifier(Constants.QUOTES),
+                        new PrimaryDrawerItem().withName("Authors").withIcon(FontAwesome.Icon.faw_user).withIdentifier(Constants.AUTHORS),
+                        new PrimaryDrawerItem().withName("Settings").withIcon(GoogleMaterial.Icon.gmd_settings).withIdentifier(Constants.SETTINGS)
+
+                )
+                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                    @Override
+                    public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
+                        if (drawerItem != null && drawerItem instanceof Nameable){
+                            String name = ((Nameable) drawerItem).getName().getText(mActivity);
+                            toolbar.setTitle(name);
+                        }
+
+                        if (drawerItem != null){
+                            //handle on navigation drawer item
+                           // onTouchDrawer((int) drawerItem.getIdentifier());
+                        }
+                        return false;
+                    }
+                })
+                .withOnDrawerListener(new Drawer.OnDrawerListener() {
+                    @Override
+                    public void onDrawerOpened(View drawerView) {
+                        KeyboardUtil.hideKeyboard(QuoteListActivity.this);
+                    }
+
+                    @Override
+                    public void onDrawerClosed(View drawerView) {
+
+                    }
+
+                    @Override
+                    public void onDrawerSlide(View drawerView, float slideOffset) {
+
+                    }
+                })
+                .withFireOnInitialOnClick(true)
+                .withSavedInstance(savedInstanceState)
+                .build();
     }
 
     private void openFragment(Fragment fragment, String screenTitle){
--- app/src/main/java/com/okason/prontoquotes/utils/Constants.java
+++ app/src/main/java/com/okason/prontoquotes/utils/Constants.java
@@ -5,6 +5,12 @@ package com.okason.prontoquotes.utils;
  */
 
 public class Constants {
+    public final static int QUOTES = 1;
+    public final static int AUTHORS = 2;
+    public final static int SETTINGS = 3;
+    public final static int LOGOUT = 4;
+    public final static int ADD  = 5;
+
     public static final String REALM_DATABASE = "pronto_quote.realm";
     public static final String FIRST_RUN = "first_run";
 }
